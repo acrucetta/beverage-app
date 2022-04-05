ae3edb2c91cc9eb632a61ca86cef305a23662af6
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2022-04-05 17:53:09 -0500

    Initial commit

---
+++ ThirstrApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThirstrApp
+//
+//  Created by Chelsea Troy on 4/12/20.
+//  Copyright © 2020 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
